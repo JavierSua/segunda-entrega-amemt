471a2eaef15b8c32c84bde3affa535a384f04c13
JavierSua <Javier.su.96@gmail.com>
2020-12-26 15:22:37 -0300

    funcionalidad al boton agregar elemnto de tabla. boton sin func de borrar elem tabla.

--- ejecutables/tabla.js
+++ ejecutables/tabla.js
@@ -2,27 +2,33 @@
 
 let createHeader = (claves) => {
     
-    let theadEl = document.createElement("thead");
-    let trEl = document.createElement("tr");
+    let theadEl = document.createElement('thead');
+    let trEl = document.createElement('tr');
+    let thEl = document.createElement('th');
     
     claves.forEach(element => {
-        let thEl = document.createElement("th");
-        thEl.classList.add()
+        let thEl = document.createElement('th');
         thEl.innerHTML = element;
         trEl.appendChild(thEl);  
     });
+    thEl.innerHTML = 'Borrar';
+    trEl.appendChild(thEl);  
 
     theadEl.appendChild(trEl);
     tableEl.appendChild(theadEl);
 }
 
 let createRow = (element) => {
-    let trEl = document.createElement("tr");
+    let trEl = document.createElement('tr');
     for (let clave in element) {
-        let tdEl = document.createElement("td");
+        let tdEl = document.createElement('td');
         tdEl.innerHTML = element[clave];
         trEl.appendChild(tdEl)
     }
+    let tdEl = document.createElement('BUTTON');
+    tdEl.innerHTML = 'X';
+    tdEl.classList.add('btn-borrar');
+    trEl.appendChild(tdEl);
     return trEl
 }
 
@@ -34,7 +40,14 @@ let createBody = (elements)=> {
     tableEl.appendChild(tbodyEl);
 };
 
+const clearArray = (array) => {
+    Array.from(array).forEach((el) => {
+        el.value = "";
+    });
+};
+
 let btnAgregar = document.getElementById('btnTablaModalAgregar');
+let modalInputArray = document.getElementsByClassName('modalInput');
 
 window.addEventListener('load', () => {
     createHeader(clavesEspecimenes);
@@ -42,5 +55,16 @@ window.addEventListener('load', () => {
 });
 
 btnAgregar.addEventListener('click', () => {
-    console.log("oli")
+    let tbodyEl = document.getElementsByTagName('tbody')[0];
+    
+    let NuevoEspecimen = {
+        Nombre : modalInputArray[0].value,
+        Clima : modalInputArray[1].value,
+        Flor : modalInputArray[2].value,
+        Fruta : modalInputArray[3].value,
+        Hojas : modalInputArray[4].value
+    } 
+    tbodyEl.appendChild(createRow(NuevoEspecimen));
+    clearArray(modalInputArray);
+    $('#exampleModal').modal('hide');
 });
